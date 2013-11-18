61f0af867a2b81eb7dc6d8356f8589f83c1dbfac
frecel <artur@fracek.com>
2013-11-17 17:57:23 -0700

    CSP compliency fixes

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+function onLoadEvents() {
+    httpRequest();
+    document.getElementById("first").onclick = function () { httpRequest(1); };
+    document.getElementById("previous").mousedown = function () { httpRequest(window.currentxkcd - 1); };
+    document.getElementById("random").onclick = function () { httpRequest(Math.round(Math.random() * window.xkcdlatest + 1)); };
+    document.getElementById("next").onclick = function () { httpRequest(window.currentxkcd + 1); };
+    document.getElementById("last").onclick = function () { httpRequest(); };
+    document.getElementById("alt").onclick = function () { window.alert(window.xkcdinfo.alt); };
+}
+
 /*
 load() function directs Element with the id "comic" to
 window.xkcdinfo.img which contains the current image location
 */
 function load() {
     window.currentxkcd = window.xkcdinfo.num;
-    document.getElementById("comic").innerHTML = "<img src=" + window.xkcdinfo.img + ">";    
+    document.getElementById("comic").innerHTML = "<img src=" + window.xkcdinfo.img + ">";
 }
 
 /*
@@ -13,10 +23,7 @@ for the most current JSON file containing the Image and alternate information
 for the current XKCD comic which it then strips and assigns to window.xkcdinfo.num
 for later use with load function.
 */
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/Development-Branch
 function httpRequest(strip) {
     var reqxkcd;
     reqxkcd = new XMLHttpRequest({mozSystem: true});
@@ -27,16 +34,28 @@ function httpRequest(strip) {
                 window.xkcdlatest = window.xkcdinfo.num;
             }
             load();
-        }
-		else {
+        } else {
             document.getElementById("comic").innerHTML = reqxkcd.readyState + " " + reqxkcd.status;
         }
-        };
+    };
     if (!strip) {
         reqxkcd.open("GET", "http://xkcd.com/info.0.json", true);
-    }
-	else {
+    } else {
         reqxkcd.open("GET", "http://xkcd.com/" + strip + "/info.0.json", true);
     }
-    reqxkcd.send(null);    
+    reqxkcd.send(null);
 }
+
+/* Event Listeners*/
+window.onload = function () { onLoadEvents(); };
+
+
+
+
+/*
+function getRandomNumber() {
+    return 4; //chosen by fair dice roll
+              //guaranteed to be random
+}
+*/
+
